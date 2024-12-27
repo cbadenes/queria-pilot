af71fc84659b8ff647cf851e3ba89958a2032f8a
cbadenes <cbadenes@gmail.com>
2024-12-27 11:59:44 +0100

    new models

---
+++ admin/models/create_models.sh
@@ -0,0 +1,8 @@
+ollama create llama3_easy -f ./modelfile/modelfile_easy
+ollama create llama3_medium -f ./modelfile/modelfile_medium
+ollama create llama3_hard -f ./modelfile/modelfile_hard
+
+ollama create llama3_easy_Open -f ./modelfile/modelfile_easy_Openquestion
+ollama create llama3_medium_Open -f ./modelfile/modelfile_medium_Openquestion
+ollama create llama3_hard_Open -f ./modelfile/modelfile_hard_Openquestion
+ollama create llama3_evaluator -f ./modelfile/modelfile_evaluator
