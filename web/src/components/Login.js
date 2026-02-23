b021b3ee62200635526499399e079581398e3e67
cbadenes <cbadenes@gmail.com>
2026-02-23 09:46:51 +0100

    relative URL from API

--- web/src/components/Login.js
+++ web/src/components/Login.js
@@ -37,7 +37,7 @@ const Login = () => {
       const loginData = { email, password };
 
       try {
-        const response = await fetch(`${API_BASE_URL}/login`, {
+        const response = await fetch(`${API_BASE_URL}/api/login`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(loginData)
--- web/src/components/config.js
+++ web/src/components/config.js
@@ -1,5 +1,5 @@
 // src/config.js
-const API_BASE_URL = "/queria/api";
+const API_BASE_URL = "/queria";
 export default API_BASE_URL;
 //const API_BASE_URL = 'http://127.0.0.1:3500';
 //const API_BASE_URL = 'https://queria.etsisi.upm.es/api';
